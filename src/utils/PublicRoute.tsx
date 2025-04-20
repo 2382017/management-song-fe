b1b877e370eedf5ec2e37da6e5614dea7f5b53ce
2382017 <2382017@unai.edu>
2025-04-20 21:46:08 +0700

    Handson-11

---
+++ src/utils/PublicRoute.tsx
@@ -0,0 +1,12 @@
+
+import { ReactNode } from 'react';
+import { useAuth } from './AuthProvider';
+import { Navigate } from 'react-router-dom';
+
+const PublicRoute = ({children} : {children : ReactNode}) => {
+    const {isAuthenticated} = useAuth();
+    return isAuthenticated ? <Navigate to="/" /> : children;
+}
+
+export default PublicRoute
+
